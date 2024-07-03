47b937d420640d4bb34616a4438533c75adc23b0
Prashant Solanki <prashantsolanki2025@gmail.com>
2024-07-03 20:37:24 +0530

    Responsive design for lg screens

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 function Body() {
   return (
-    <div>
+    <div className="mt-10">
       <section className="container mx-auto">
         <div className="flex justify-center items-center space-x-20">
           <div className="flex flex-col gap-10">
-            <div className="flex flex-col gap-3 ">
-              <h1 className="font-quicksand font-bold leading-tight text-5xl">
+            <div className="flex flex-col gap-3 order-1">
+              <h1 className="text-5xl font-quicksand font-bold leading-tight">
                 Best way to revise for your next coding interview
               </h1>
               <p className="font-roboto tracking-wider text-gray-600 text-xl">
@@ -19,7 +19,7 @@ function Body() {
                 Buy <span className="uppercase">Free</span> through
               </button>
             </div>
-            <div className="mt-5 px-10 py-6 bg-sky-50 border-l-8 border-orange-400">
+            <div className="mt-5 px-10 py-6 bg-sky-50 border-l-8 border-orange-400 order-2">
               <p>
                 <i>
                   "Highly recommended if you're interviewing or preparing these
@@ -34,75 +34,174 @@ function Body() {
           </div>
         </div>
       </section>
-      <section className="container mx-auto mt-20">
+      <section className="container mx-auto mt-32">
         <div className="flex flex-col justify-center items-center">
-          <h2 className="font-quicksand font-bold text-3xl">
-            What's included?
+          <h2 className="font-quicksand font-bold text-4xl">
+            What's Included?
           </h2>
-          <div className="flex gap-8 mt-8">
+          <div className="flex gap-8 mt-10">
             <div>
               <img
                 src="./assets/images/dsa-2.png"
                 alt="Book Table Contents"
-                className="w-80"
+                className="w-96"
               />
             </div>
-            <div>
-              <div className="flex space-x-2">
-                <img
-                  src="./assets/images/checkmark.png"
-                  alt="checkmark"
-                  className="w-5"
-                />
-                <p>15+ algo patterns discussed</p>
+            <div className="flex flex-col items-center">
+              <ul className="list-none space-y-3">
+                <li className="flex space-x-2">
+                  <img
+                    src="./assets/images/checkmark.png"
+                    alt="Checkmark"
+                    className="h-6 w-auto"
+                  />
+                  <p className="text-gray-500 text-xl">
+                    15+ algo patterns discussed
+                  </p>
+                </li>
+                <li className="flex space-x-2">
+                  <img
+                    src="./assets/images/checkmark.png"
+                    alt="Checkmark"
+                    className="h-6 w-auto"
+                  />
+                  <p className="text-gray-500 text-xl">
+                    Detailed solutions and explanations
+                  </p>
+                </li>
+                <li className="flex space-x-2">
+                  <img
+                    src="./assets/images/checkmark.png"
+                    alt="Checkmark"
+                    className="h-6 w-auto"
+                  />
+                  <p className="text-gray-500 text-xl">
+                    Awesome resources, articles, blogs
+                  </p>
+                </li>
+                <li className="flex space-x-2">
+                  <img
+                    src="./assets/images/checkmark.png"
+                    alt="Checkmark"
+                    className="h-6 w-auto"
+                  />
+                  <p className="text-gray-500 text-xl">
+                    Visualizers and animations included
+                  </p>
+                </li>
+                <li className="flex space-x-2">
+                  <img
+                    src="./assets/images/checkmark.png"
+                    alt="Checkmark"
+                    className="h-6 w-auto"
+                  />
+                  <p className="text-gray-500 text-xl">
+                    100 pages of quality content
+                  </p>
+                </li>
+                <li className="flex space-x-2">
+                  <img
+                    src="./assets/images/checkmark.png"
+                    alt="Checkmark"
+                    className="h-6 w-auto"
+                  />
+                  <p className="text-gray-500 text-xl">
+                    Concepts explained for beginners
+                  </p>
+                </li>
+                <li className="flex space-x-2">
+                  <img
+                    src="./assets/images/checkmark.png"
+                    alt="Checkmark"
+                    className="h-6 w-auto"
+                  />
+                  <p className="text-gray-500 text-xl">
+                    All in one revision guide
+                  </p>
+                </li>
+              </ul>
+              <button className="bg-orange-500 px-6 py-3 rounded-3xl text-white font-bold w-2/3 mt-10 ">
+                Download for Free
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="container mx-auto mt-20">
+        <div className="grid grid-rows-2 grid-cols-2 gap-5">
+          <div className="flex flex-col gap-4 border border-gray-200 rounded-xl px-7 py-10">
+            <q>
+              Walling helps us visually organise ideas and tasks, work
+              collaboratively and efficiently within our team. The visual
+              experience makes it so easy to plan content and stay on track with
+              our projects and campaigns.
+            </q>
+            <div className="flex gap-4 items-center">
+              <img
+                src="./assets/images/profiles/profile-1.png"
+                alt="Author Image"
+                className="h-20"
+              />
+              <div className="flex flex-col">
+                <p>Mathew Andres</p>
+                <p>Head of Designing</p>
               </div>
-              <div className="flex space-x-2">
-                <img
-                  src="./assets/images/checkmark.png"
-                  alt="checkmark"
-                  className="w-5"
-                />
-                <p>Detailed solutions and explanations</p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-4 border border-gray-200 rounded-xl px-7 py-10">
+            <q>
+              Walling helps us visually organise ideas and tasks, work
+              collaboratively and efficiently within our team. The visual
+              experience makes it so easy to plan content and stay on track with
+              our projects and campaigns.
+            </q>
+            <div className="flex gap-4 items-center">
+              <img
+                src="./assets/images/profiles/profile-1.png"
+                alt="Author Image"
+                className="h-20"
+              />
+              <div className="flex flex-col">
+                <p>Mathew Andres</p>
+                <p>Head of Designing</p>
               </div>
-              <div className="flex space-x-2">
-                <img
-                  src="./assets/images/checkmark.png"
-                  alt="checkmark"
-                  className="w-5"
-                />
-                <p>Awesome resources, articles, blogs</p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-4 border border-gray-200 rounded-xl px-7 py-10">
+            <q>
+              Walling helps us visually organise ideas and tasks, work
+              collaboratively and efficiently within our team. The visual
+              experience makes it so easy to plan content and stay on track with
+              our projects and campaigns.
+            </q>
+            <div className="flex gap-4 items-center">
+              <img
+                src="./assets/images/profiles/profile-1.png"
+                alt="Author Image"
+                className="h-20"
+              />
+              <div className="flex flex-col">
+                <p>Mathew Andres</p>
+                <p>Head of Designing</p>
               </div>
-              <div className="flex space-x-2">
-                <img
-                  src="./assets/images/checkmark.png"
-                  alt="checkmark"
-                  className="w-5"
-                />
-                <p>Visualizers and animations included</p>
-              </div>
-              <div className="flex space-x-2">
-                <img
-                  src="./assets/images/checkmark.png"
-                  alt="checkmark"
-                  className="w-5"
-                />
-                <p>100 pages of quality content</p>
-              </div>
-              <div className="flex space-x-2">
-                <img
-                  src="./assets/images/checkmark.png"
-                  alt="checkmark"
-                  className="w-5"
-                />
-                <p>Concepts explained for beginners</p>
-              </div>
-              <div className="flex space-x-2">
-                <img
-                  src="./assets/images/checkmark.png"
-                  alt="checkmark"
-                  className="w-5"
-                />
-                <p>All in one revision guide</p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-4 border border-gray-200 rounded-xl px-7 py-10">
+            <q>
+              Walling helps us visually organise ideas and tasks, work
+              collaboratively and efficiently within our team. The visual
+              experience makes it so easy to plan content and stay on track with
+              our projects and campaigns.
+            </q>
+            <div className="flex gap-4 items-center">
+              <img
+                src="./assets/images/profiles/profile-1.png"
+                alt="Author Image"
+                className="h-20"
+              />
+              <div className="flex flex-col">
+                <p>Mathew Andres</p>
+                <p>Head of Designing</p>
               </div>
             </div>
           </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
+  const year = new Date().getFullYear();
   return (
-    <div>Footer</div>
-  )
+    <div className="flex justify-center mt-20 mb-10">
+      <p>
+        Designed with ❤️ by
+        <a href="https://github.com/Prashant-2024">Prashant</a>
+      </p>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
